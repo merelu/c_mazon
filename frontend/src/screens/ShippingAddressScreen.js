fd0044f2ba1481a9968ee0c6a15a145a0c359ee4
GYUHA_HWANG <gyuha0310@gmail.com>
2021-01-02 22:57:55 +0900

    ShippingAddressScreen 작성

---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import CheckoutSteps from "../components/CheckoutSteps";
+
+export default function ShippingAddressScreen() {
+  const [fullName, setFullName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // dispatch(saveShippingAddress({fullName, address, city, postalCode, country}));
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Shipping Address</h1>
+        </div>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Enter full name"
+            value={fullName}
+            onchange={(e) => setFullName(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter Address"
+            value={address}
+            onchange={(e) => setAddress(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            placeholder="Enter city"
+            value={city}
+            onchange={(e) => setCity(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            type="text"
+            id="postalCode"
+            placeholder="Enter postal code"
+            value={postalCode}
+            onchange={(e) => setPostalCode(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            placeholder="Enter country"
+            value={country}
+            onchange={(e) => setCountry(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
